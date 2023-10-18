014fc9d65aba8a619b1745d261acb25bdb0cdfbe
Won,Heeyeon <whyp1028@gmail.com>
2023-10-18 10:24:24 +0900

    three

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ package f201913027;
 public class Main { 
 
     public static void main(String[] args) { 
-        System.out.println("one 201913027"); 
+        System.out.println("one 201913027");
+        Three.print(); 
     } }
---
+++ Three.java
@@ -0,0 +1,6 @@
+package f201913027; 
+
+public class Three { 
+    public static void print() { 
+        System.out.println("three"); 
+    } }
